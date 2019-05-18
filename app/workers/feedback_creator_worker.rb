7fee7d5732df108a0392eee7388df7b3bb4f487a
ibrahimelbanna <proghima54@gmail.com>
2019-05-18 03:37:06 +0200

    Adding testing for the workers

--- app/workers/feedback_creator_worker.rb
+++ app/workers/feedback_creator_worker.rb
@@ -3,6 +3,7 @@ class FeedbackCreatorWorker
   sidekiq_options retry: false
   
   def perform(feedback_params, state_params)
+    byebug
     @feedback = Feedback.create!(eval(feedback_params))
     @feedback.create_state!(eval(state_params))
   end
