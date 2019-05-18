af66cf9d4eaf4f8fd8809d8082cea880920ab5de
ibrahimelbanna <proghima54@gmail.com>
2019-05-18 12:34:19 +0200

    Deleting coverage dir

--- app/workers/feedback_creator_worker.rb
+++ app/workers/feedback_creator_worker.rb
@@ -3,7 +3,6 @@ class FeedbackCreatorWorker
   sidekiq_options retry: false
   
   def perform(feedback_params, state_params)
-    byebug
     @feedback = Feedback.create!(eval(feedback_params))
     @feedback.create_state!(eval(state_params))
   end
