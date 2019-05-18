371c28b07ec063f3ed6c4898103754c9af65d964
IbrahimMohamed <proghima54@gmail.com>
2019-05-18 04:29:32 +0200

    Adding testing for the workers (#10)

---
+++ spec/workers/feedback_creator_worker_spec.rb
@@ -0,0 +1,23 @@
+require 'rails_helper'
+
+RSpec.describe FeedbackCreatorWorker  do
+  let!(:feedback){create(:feedback)}
+  let!(:params) do {
+    company_token: '2314124',
+    priority: "minor",
+    number: 1,
+    state:{
+     device:"LG G4", 
+     os: "Android 6.0",
+     memory: 2048, 
+     storage: 4096,
+    }
+   }
+  end
+  it "Should run worker" do
+    expect {
+      FeedbackCreatorWorker.perform_async(params.except(:state), params[:state]
+      )
+    }.to change(FeedbackCreatorWorker.jobs, :size).by(1)
+  end
+end
