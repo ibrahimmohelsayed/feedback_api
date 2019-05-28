521cdd8c9071402491028a8260e4229154a7b1cd
ibrahimelbanna <imohamed@lynks.com>
2019-05-28 13:08:29 +0200

    Adding more tests for the models and validations and associations

--- spec/models/feedback_spec.rb
+++ spec/models/feedback_spec.rb
@@ -1,5 +1,24 @@
 require 'rails_helper'
 
 RSpec.describe Feedback, type: :model do
-  it { should have_one(:state) }
+  it 'has a valid factory' do
+    # Check that the factory we created is valid
+    expect(build(:feedback)).to be_valid
+  end
+  let(:attributes) do
+    {
+    company_token: '2314124',
+    priority: "minor",
+    number: 1,
+  }
+  end
+  let(:feedback) { create(:feedback, **attributes) }
+  describe 'model validations' do
+    it { expect(feedback).to allow_value(attributes[:company_token]).for(:company_token) }
+    it { expect(feedback).to allow_value(attributes[:priority]).for(:priority) }
+    it { expect(feedback).to allow_value(attributes[:number]).for(:number) }
+  end
+  describe 'model associations' do
+    it { should have_one(:state) }
+  end  
 end
--- spec/models/state_spec.rb
+++ spec/models/state_spec.rb
@@ -1,5 +1,25 @@
 require 'rails_helper'
 
 RSpec.describe State, type: :model do
-  it { should belong_to(:feedback) } 
+  it 'has a valid factory' do
+    expect(build(:state)).to be_valid
+  end
+  let(:attributes) do
+    {
+      device:"LG G4", 
+      os: "Android 6.0",
+      memory: 2048, 
+      storage: 4096,
+    }
+  end
+  let(:state) { create(:state, **attributes) }
+  describe 'model validations' do
+    it { expect(state).to allow_value(attributes[:device]).for(:device) }
+    it { expect(state).to allow_value(attributes[:os]).for(:os) }
+    it { expect(state).to allow_value(attributes[:memory]).for(:memory) }
+    it { expect(state).to allow_value(attributes[:storage]).for(:storage) }
+  end
+  describe 'model associations' do
+    it { should belong_to(:feedback) } 
+  end
 end
